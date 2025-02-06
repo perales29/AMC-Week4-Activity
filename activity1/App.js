cb083b6117c2045f4ff03c1354049f767d46bc51
perales29 <cliffjustine11@gmail.com>
2025-02-06 16:48:14 +0800

    ScrollView

---
+++ activity1/App.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {Image, ScrollView, Text} from 'react-native';
+
+
+
+const App = () => (
+  <ScrollView>
+    <Text>Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino ('titino)
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino
+Magdamag ka raw umiinom?
+Ah, tawag sa 'yo, "Kabit", ako naman sabik
+Na madama 'yong yakap mo, presensya mo't halik
+"Paboritong pagkakamali" tawag sa 'yo
+Sabi mo, mga kaibigan ko, ayaw sa 'yo
+Gusto nila sa may magara ka na kotse
+Lakbay sa lugar na kailangan ng pasaporte
+Habang ako ay magdamag na nakaposte
+Sa aking hood, kasi hindi pa ako boss, eh
+Nahumaling ka sa aking pagkanta
+Panay ang tanong mo sa 'kin, "Nasa'n ka?"
+Sabi ko, "Sa bahay," kahit gumigimik
+Nakita mo sa IG ko, may gumigiling
+Nahumaling ka sa aking pagkanta
+Panay ang tanong mo sa 'kin, "Nasa'n ka?"
+Sabi ko, "Sa bahay," kahit gumigimik
+Nakita mo sa IG ko, may gumigiling
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino ('titino)
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino ('titino)
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino
+Magdamag ka raw umiinom? Oh
+Gusto kitang makita kahit patago
+Sa tagpuan natin tayo magtagpo
+'Di ka nawawala sa puso at sistema ko
+Kaya pasensiya kung 'yong kuwento natin tinetema ko
+Nandiyan ka agad kapag kailangan ko
+Pero wala ako kapag kailangan mo
+Kasi madalas akong naka-airplane mode
+Ang sabi mo, "Baka kasama mo lang girlfriend mo"
+Naku, hindi, mas gusto lang kita na sabikin
+Para 'pag nagkita tayo, 'di bitin
+'Yong kalungkutan kong sakit, iyong gamutin
+"Mahal mo ba 'ko?" ang madalas mong tanungin
+'Di ko masagot, 'di pa sigurado
+Umibig ay takot, kasi natalo na 'ko
+Kaya madalas din ako na umiwas
+Mas gusto ko ay pera para mas lalong tumikas
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino ('titino)
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino
+Magdamag ka raw umiinom?
+Masakit man sa 'kin na pakawalan ka
+Mas mabuti na 'to kaysa mag-abang ka
+Kung kailan ako magtitino
+Magdamag ka raw umiinom? (YoungMLV got the sounds, bitch)</Text>
+    
+  </ScrollView>
+);
+
+export default App;
